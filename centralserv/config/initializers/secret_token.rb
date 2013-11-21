deb42cb91e6d3d4f5aad7106385e219708931a23
Jeff Chang <jeffchang.ca@gmail.com>
2013-11-21 15:58:39 +0000

    add rails app

---
+++ centralserv/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Centralserv::Application.config.secret_key_base = 'fcf58be6e908db103651ea74fe7fdfae0aad9e4a752394bccb3ab7cee7024c74dc8c00d4df7ffb54f1033928b9f76da57667072b751f70619ca6e67ad0e96971'
