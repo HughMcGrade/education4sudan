1d1111037289f1c668bece0242049714c4d8950a
Jeff Chang <jeffchang.ca@gmail.com>
2013-11-22 07:12:46 +0000

    add even more stuff

--- centralserv/app/controllers/dashboard_controller.rb
+++ centralserv/app/controllers/dashboard_controller.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class DashboardController < ApplicationController
 
   skip_before_filter :verify_authenticity_token, :except => [:index]
@@ -7,7 +9,9 @@ class DashboardController < ApplicationController
 
   def attendance
     print "!!!!!!!!!!!!!"
-    print params
+    print "?????"
+    obj = params["dashboard"].fetch("_json")[0].fetch("name")
+    print obj
   end
 
   def curriculum
@@ -27,7 +31,9 @@ class DashboardController < ApplicationController
 
   def inventory
     print "!!!!!!!!!!!!!"
-    print params
+    name = params["dashboard"].fetch("_json")[0].fetch('title')
+    description = params["dashboard"].fetch("_json")[0].fetch('title')
+    Request.create(name: name, description: description, school_id: 1)
   end
 
 end
