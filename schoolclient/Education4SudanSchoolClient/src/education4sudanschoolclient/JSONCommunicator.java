dd3c169dbeebd2e43fe24d122459b34d53210ff1
Hugh McGrade <hugh_mcgrade@hotmail.co.uk>
2013-11-22 04:35:54 +0000

    Add build

---
+++ schoolclient/Education4SudanSchoolClient/src/education4sudanschoolclient/JSONCommunicator.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package education4sudanschoolclient;
+
+/**
+ *
+ * @author Hugh
+ */
+public class JSONCommunicator {
+    private static final String ATTENDANCE_ADDRESS = "localhost:3000/attend";
+    private static final String CURRICULUM_ADDRESS = "localhost:3000/curric";
+    private static final String REQUEST_ADDRESS = "localhost:3000/reques";
+    private static final String TEACHERS_ADDRESS = "localhost:3000/teach";
+    private static final String INVENTORY_ADDRESS = "locahost:3000/invent";
+    private static final String NOTIFICATIONS_ADDRESS = "localhost:3000/notif";
+    private static final String REQUEST_STATUS_ADDRESS = "localhost:3000/request_status";
+    
+    //public static void 
+}
